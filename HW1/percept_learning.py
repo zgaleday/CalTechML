084b76dd2c3b985cc224d6e3e0e8bc3cf5f7b6bb
zgaleday <zgd3200@gmail.com>
2016-10-06 09:06:47 -0400

    add init file to allow import

--- HW1/percept_learning.py
+++ HW1/percept_learning.py
@@ -2,71 +2,7 @@ __author__ = 'zachary'
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
-"""
-Function generates a random line by choosing two points uniformly ([-1,1], [-1,1]) at random in 2D plane.
-The function returns the value of the slope and the value of the intercept to caller.
-"""
-
-
-def target_function():
-    point_a = (random.uniform(-1, 1), random.uniform(-1, 1))
-    point_b = (random.uniform(-1, 1), random.uniform(-1, 1))
-    m = (point_a[1] - point_b[1]) / (point_a[0] - point_b[0])
-    b = point_a[1] - m * point_a[0]
-    return m, b
-
-
-"""
-Function to classify points as either correct under current model or incorrect.
-Takes an input point 2D as an array as input, with w vector (in 3D) from the current hypothesis.
-Returns a boolean based on dot product of w and point (with 1 added to make array 3D).
-False if below or at threshold under give hypothesis. True if above threshold.
-"""
-
-
-def vector_classify(point, w):
-    dot = np.dot(np.array([point[0], point[1], 1.0]), w)
-    if (dot > 0):
-        return True
-    else:
-        return False
-
-
-"""
-Generates the data set to be used with the PLA.
-Takes in the number of points to generate as an argument as well as m, b of target function.
-Returns the points as an array of vectors and a boolean array to classify the points compared with a give m, b
-"""
-
-
-def generate_set(number, m, b):
-    vectors = np.random.uniform(-1, 1, (number, 2))
-    bools = np.ones(number, dtype=bool)
-    for count, vector in enumerate(vectors):
-        bools[count] = vector_classify(vector, np.array([m, - 1, b]))
-    return vectors, bools
-
-
-"""
-Takes the points generated in the generate set function and draws a scatter containing those plots.  The scatter
-also shows the target function.  This is used as a visible conformation of the boolean assignation in generate_set()
-Takes vectors from generate set and m, b for the target function as params.
-Return 1.
-"""
-
-
-def plot_points(points, bools, m, b):
-    plt.plot([((-1 - b) / m), ((1 - b) / m)], [-1, 1], 'r')
-    for count, point in enumerate(points):
-        if bools[count]:
-            plt.plot(point[0], point[1], 'bo')
-        else:
-            plt.plot(point[0], point[1], 'ro')
-    plt.ylim([-1, 1])
-    plt.xlim([-1, 1])
-    plt.show()
-
+from DataSet import DataSet
 
 """
 Takes two vectors as input.  The w vector or hypothesis vector and the misclassified point vector.
@@ -76,12 +12,12 @@ and target function.
 """
 
 
-def update(w, x, bools, index):
-    if bools[index]:
+def update(h, data_set, index):
+    if data_set.bools[index]:
         d = 1
     else:
         d = -1
-    temp = np.add(w, np.multiply(d, x))
+    temp = np.add(w, np.multiply(d, data_set.points[index]))
     return temp
 
 
@@ -96,8 +32,9 @@ If toggle False: final hypothesis.
 """
 
 
-def pla(points, bools, w, toggle="time"):
+def pla(number, w, toggle="time"):
     t_converge = 0
+    data_set = DataSet(number)
     while (True):
         check = False
         for count, point in enumerate(points):
