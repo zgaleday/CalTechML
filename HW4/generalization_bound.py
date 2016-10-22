58fc2acd0cfd94cea1255d6556f7a7370cf2f083
zgaleday <zgd3200@gmail.com>
2016-10-22 15:36:20 -0400

    Add code skeleton for generalization bound.

---
+++ HW4/generalization_bound.py
@@ -0,0 +1,29 @@
+def delta(n, epsilon):
+
+    """
+    Calculated the delta constant in the generalization bound
+    :param n: Number of points (> 0)
+    :param epsilon: epsilon of generalization function
+    :return: the delta value in generalization bound
+    """
+
+
+def simple_growth(n, dvc):
+
+    """
+    A simplified growth function for the generalization bound m_h(N) = N^dvc.
+    :param n: Number of points in the data set (> 0)
+    :param dvc: The VC dimension of the hypothesis set (> 0)
+    :return: value of simplified growth function
+    """
+
+def omega(n, epsilon, dvc):
+
+    """
+    Calculates the omega of the generalization bound using delta and simple growth.
+    :param n: see delta and simple_growth
+    :param epsilon: see dela
+    :param dvc: see simple growth
+    :return: the omega value of the generalization bound
+    """
+
