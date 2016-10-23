64eac10312f3d6ad3e38752a237a1cdf012e7d10
zgaleday <zgd3200@gmail.com>
2016-10-22 22:12:35 -0400

    add target fnct method

--- HW4/bias_variance.py
+++ HW4/bias_variance.py
@@ -13,6 +13,19 @@ def sine_compare(x, y):
     else:
         return 1
 
+
+def avg_compare(avg, x, y):
+    """
+    Classifies points compared to avg * x function
+    :param x: x coord
+    :param y: y coord
+    :return: -1 if below or equal 1 otherwise
+    """
+    if avg * x <= y:
+        return -1
+    else:
+        return 1
+
 def linear_regression(points):
 
     """
@@ -33,3 +46,37 @@ def linear_regression(points):
     return a
 
 
+def repeats(trials):
+
+    """
+    Runs linear_regression trials times recording the a values and returing an array of the values of lenght trials
+    :param trials: number of trials
+    :return: array of trials length with a values
+    """
+    result = np.empty(trials)
+    for trial in range(trials):
+        temp = np.random.uniform(-1, 1, 4)
+        temp[1] = np.sin(np.pi * temp[0])
+        temp[3] = np.sin(np.pi * temp[2])
+        result[trial] = linear_regression(np.random.uniform(-1, 1, 4))
+
+    return result
+
+def var(result, avg):
+
+    """
+    Calculates the variance of the ax solution
+    :param result: array of a's from repeats
+    :param avg: average hypothesis
+    :return: the expected value of the variance
+    """
+    expected = 0.0
+    for count, a in enumerate(result):
+        expected = (expected * count + (a - avg) ** 2) / (count + 1)
+    return expected
+
+a = repeats(100000)
+average = np.average(a)
+variance = var(a, average)
+print("Average: ", average)
+print("Variance: ", variance)
