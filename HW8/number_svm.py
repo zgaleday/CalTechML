ffd1bda7fb336d4146a41780d0db52108eafc0c0
zgaleday <zgd3200@gmail.com>
2016-11-16 22:38:20 -0500

    Write method headers and specifications for number_svm

---
+++ HW8/number_svm.py
@@ -0,0 +1,91 @@
+"""
+A class to run soft-margin SVM on the features data set included (from postal service data set of zip codes).
+Dependencies:  scikit learn and numpy
+"""
+import numpy as np
+from sklearn import svm
+
+
+class NumberSVM:
+
+    """
+    Intakes and cleans the data from features.train for training using various user defined SVM parameters
+    and Kernal methods.
+    """
+
+    def __init__(self):
+        """
+        Defines instance variable for use in later methods
+        """
+        self.X = np.empty(dtype='d')
+        self.numbers = np.empty()
+        self.Y = np.empty(dtype='d')
+        self.svm = None
+        self.text_X = np.empty(dtype='d')
+        self.test_numbers = np.empty()
+        self.test_Y = np.empty(dtype='d')
+
+    def read_data(self, filename, test=False):
+        """
+        Reads in data points from filename to instance arrays.  If test is false reads into self.X and self.numbers else
+        reads into self.test_X and self.test_numbers.  Data is formatted with number in first col.  With features in
+        next two, no header.
+        :param filename: File in which data is located
+        :param test: toggle to select whether set being read in is training or test test. If true test, else training
+        :return: void
+        """
+        # TODO
+
+    def number_v_all(self, number, test=False):
+        """
+        Sets the target vector (either self.Y or self.test_Y).  Assigns +1 to any Y[i] where self.numbers[i] == number
+        and -1 otherwise.
+        :param number: number to set classification to 1 for
+        :param test: toggle for training or in sample (true for setting test_Y false for Y)
+        :return: void
+        """
+        # TODO
+
+    def number_v_number(self, a, b, test=False):
+        """
+        Sets Y[i] = +1 if  numbers[i] == a, Y[i] == -1 if numbers[i] ==b and 0 otherwise
+        :param a: number to set classification to +1 for
+        :param b: number to set classification to -1 for
+        :param test: see methods above
+        :return: void
+        """
+        # TODO
+
+    def poly_solver(self, Q, C):
+        """
+        Method for solving the polynomial kernel soft-margin SVM problem
+        :param Q: Order of the polynomial kernel to be used
+        :param C: Margin violation constraint
+        :return: void
+        """
+        # TODO
+
+    def poly_cross_validation(self, Q, C):
+        """
+        Runs SVM with 10-fold cross validation
+        :param Q: Order of the polynomial kernel to be used
+        :param C: Margin violation constraint
+        :return: Error of the cross validation
+        """
+        # TODO
+
+    def error_in(self):
+        """
+        Method for determining the in sample error under the current SVM instance. If the current SVM instance is None
+        returns -1
+        :return: Error in measure if SVM exists -1 otherwise
+        """
+        # TODO
+
+    def error_out(self):
+        """
+        Method for determining the out-of-sample error under the current SVM instance. If the current SVM instance is
+        None returns -1
+        :return: Out-of-sample error measure if SVM exists -1 otherwise
+        """
+        # TODO
