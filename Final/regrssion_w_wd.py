6dda82aac71ba37fbef29c77e890d4c114106a29
zgaleday <zgd3200@gmail.com>
2016-11-25 14:44:29 -0500

    Modify method to solve problems 7-9

--- Final/regrssion_w_wd.py
+++ Final/regrssion_w_wd.py
@@ -16,11 +16,15 @@ def problems_7_through_9(transform=False):
         data.number_v_all(number)
         data.number_v_all(number, test=True)
         if transform:
-            data.X = LRC.transform(data.X)
-            data.test_X = LRC.transform(data.test_X)
-        g = LRC.weight_decay_lr_classification(data.X, data.Y, 0)
-        ein = LRC.classification_error(data.X, data.Y, g)
-        eout = LRC.classification_error(data.test_X, data.test_Y, g)
+            transformed_X = LRC.transform(data.X)
+            transformed_test_X = LRC.transform(data.test_X)
+            g = LRC.weight_decay_lr_classification(transformed_X, data.Y, 0)
+            ein = LRC.classification_error(transformed_X, data.Y, g)
+            eout = LRC.classification_error(transformed_test_X, data.test_Y, g)
+        else:
+            g = LRC.weight_decay_lr_classification(data.X, data.Y, 0)
+            ein = LRC.classification_error(data.X, data.Y, g)
+            eout = LRC.classification_error(data.test_X, data.test_Y, g)
         print("Number: {0}, Ein: {1}, Eout: {2}".format(number, ein, eout))
 
-problems_7_through_9()
+problems_7_through_9(transform=True)
