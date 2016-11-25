29122aaff114120c56ed9f52061f5aa30cea63b0
zgaleday <zgd3200@gmail.com>
2016-11-25 15:40:13 -0500

    Implement method to do transform of x into z-space

--- Final/svm_transform.py
+++ Final/svm_transform.py
@@ -13,3 +13,20 @@ def x_matrix():
                   [0, -2],
                   [-2, 0]], dtype='d')
     return x
+
+
+def transform(x):
+    """
+    Takes a matrix with points = {x1, x2} to the z space {x2^2 - 2x1 - 1, x1^2 -2x2 + 1}
+    :param x: matrix x formatted as stated above.
+    :return: z matrix
+    """
+    z = np.array((len(x), 2), dtype='d')
+    for i, point in enumerate(x):
+        x1 = point[0]
+        x2 = point[1]
+        z[i][0] = x2 ** 2 - 2 * x1 - 1
+        z[i][1] = x1 ** 2 - 2 * x2 + 1
+    return z
+
+
